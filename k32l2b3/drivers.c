d7e6cbc220d15258f5debb181bff08a7e6e1348a
Pedro Botelho <pedrobotelho15@alu.ufc.br>
2022-06-23 20:52:08 -0300

    Commented on-board drivers (LEDs, Buttons and Clock).

--- k32l2b3/drivers.c
+++ k32l2b3/drivers.c
@@ -1,49 +1,68 @@
-#include <k32_api/clock.h>
-#include <k32_api/drivers.h>
-#include <k32_api/gpio.h>
-#include <k32_api/port.h>
+#include <k32l2b3/clock.h>
+#include <k32l2b3/drivers.h>
+#include <k32l2b3/gpio.h>
+#include <k32l2b3/port.h>
 #include <stdbool.h>
 
+// =========================================================================
+// PRIVATE STATIC VARIABLES
+// =========================================================================
+
+static pin_handler_t drv_led_power;
 static pin_handler_t drv_led_green;
 static pin_handler_t drv_led_red;
 
 static pin_handler_t drv_button_sw1;
 static pin_handler_t drv_button_sw3;
 
-long drvCpuClockCounts() {
-	switch(clkGetMainClock()) {
-		case 48000000L:
-			return 4000L;
-		// TODO
-	}
-	return 0L;
-}
+// =========================================================================
+// PRIVATE FUNCTIONS PROTOTYPES
+// =========================================================================
 
 /*!
- * @brief Inits the clock of the processor at 48 MHz.
+ * @brief Get the pin handler for the given LED ID.
+ * @param led The desired on-board LED ID.
+ * @retval The pin handler of the given LED.
+ */
+pin_handler_t *drvGetLed(drv_led_id led);
+
+/*!
+ * @brief Get the pin handler for the given button ID.
+ * @param button The desired on-board button ID.
+ * @retval The pin handler of the given button.
+ */
+pin_handler_t *drvGetButton(drv_button_id button);
+
+// =========================================================================
+// PUBLIC FUNCTIONS IMPLEMENTATIONS
+// =========================================================================
+
+/*!
+ * @brief Initializes the CPU clock as 48 MHz.
  */
 void drvCpuClockInit(void) {
-	clkEnableClock(clkSOURCE_LIRCLK_8MHz);
-	clkSetMainClock(clkSOURCE_LIRCLK_8MHz);
+	clkEnableClock(clkSOURCE_HIRCLK);
+	clkSetMainClock(clkSOURCE_HIRCLK);
 }
 
 /*!
- * @brief Inits the two embedded LEDs of the board.
+ * @brief Initializes the two on-board LEDS (green and red).
  */
 void drvLedInit(void) {
+
 	drv_led_green.port = pinPORT_D;
 	drv_led_green.pin = 5;
 	gpioPinInit(&drv_led_green, gpioOUTPUT);
-	gpioPinWrite(&drv_led_green, DRV_LED_OFF);
+	gpioPinWrite(&drv_led_green, drvLED_OFF);
 
 	drv_led_red.port = pinPORT_E;
 	drv_led_red.pin = 31;
 	gpioPinInit(&drv_led_red, gpioOUTPUT);
-	gpioPinWrite(&drv_led_red, DRV_LED_OFF);
+	gpioPinWrite(&drv_led_red, drvLED_OFF);
 }
 
 /*!
- * @brief Inits the two embedded buttons of the board.
+ * @brief Initializes the two on-board buttons (SW1 and SW3).
  */
 void drvButtonInit(void) {
 	drv_button_sw1.port = pinPORT_A;
@@ -58,71 +77,106 @@ void drvButtonInit(void) {
 }
 
 /*!
- * @brief Performs a given function with the given LED.
+ * @brief Sets the LED level based on a given function.
+ * @param led The desired on-board LED ID.
+ * @param function The desired function to perform.
  */
 void drvLedSet(drv_led_id led, drv_led_function function) {
-	if(led > DRV_LED_RED || led < DRV_LED_GREEN) {
-		while(1);
-	}
-	pin_handler_t *led_handle = (led == DRV_LED_GREEN) ? &drv_led_green : &drv_led_red;
+	pin_handler_t *led_handle = drvGetLed(led);
 	switch(function) {
-		case DRV_LED_SET:
-			gpioPinWrite(led_handle, DRV_LED_ON);
+		case drvLED_SET:
+			gpioPinWrite(led_handle, drvLED_ON);
 			break;
-		case DRV_LED_CLEAR:
-			gpioPinWrite(led_handle, DRV_LED_OFF);
+		case drvLED_CLEAR:
+			gpioPinWrite(led_handle, drvLED_OFF);
 			break;
-		case DRV_LED_TOGGLE:
+		case drvLED_TOGGLE:
 			gpioPinToggle(led_handle);
 			break;
+		default:
+			while(1);
 	}
 }
 
-drv_button_level drvCheckButtonPressed(drv_button_id button) {
-	if(button > DRV_BUTTON_SW3 || button < DRV_BUTTON_SW1) {
-		while(1);
-	}
-	pin_handler_t *button_handle = (button == DRV_BUTTON_SW1) ? &drv_button_sw1 : &drv_button_sw3;
+/*!
+ * @brief Checks the given on-board button level, via polling.
+ * @param button The on-board button ID to check.
+ * @retval "drvBUTTON_PRESSED" if the button have been pressed, or
+ * "drvBUTTON_NOT_PRESSED" otherwise.
+ */
+drv_button_level drvCheckButtonLevel(drv_button_id button) {
+	pin_handler_t *button_handle = drvGetButton(button);
 	return gpioPinRead(button_handle);
 }
 
-void drvTestLeds(void) {
-	for(int i = 0; i < 10; i++) {
-		drvLedSet(DRV_LED_RED, DRV_LED_SET);
-		drvLedSet(DRV_LED_GREEN, DRV_LED_SET);
-		for(int j = 0; j < 100*drvCpuClockCounts(); j++);
-
-		drvLedSet(DRV_LED_RED, DRV_LED_CLEAR);
-		drvLedSet(DRV_LED_GREEN, DRV_LED_CLEAR);
-		for(int j = 0; j < 100*drvCpuClockCounts(); j++);
-	}
+/*!
+ * @brief Checks whether the given on-board button have been
+ * pressed, via polling.
+ * @param button The on-board button ID to check.
+ * @retval "true" if the button have been pressed, of
+ * "false" otherwise.
+ */
+bool drvCheckButtonPressed(drv_button_id button) {
+	return !drvCheckButtonLevel(button);
 }
 
-void drvTestButtons(void) {
-	bool sw1_double_tap = false;
-	bool sw3_double_tap = false;
-	while(1) {
-		if(drvCheckButtonPressed(DRV_BUTTON_SW1) == DRV_BUTTON_PRESSED) {
-			if(!sw1_double_tap) {
-				sw1_double_tap = true;
-				sw3_double_tap = false;
-				drvLedSet(DRV_LED_GREEN, DRV_LED_TOGGLE);
-			}
-			else {
-				break;
-			}
-		}
-		if(drvCheckButtonPressed(DRV_BUTTON_SW3) == DRV_BUTTON_PRESSED) {
-			if(!sw3_double_tap) {
-				sw3_double_tap = true;
-				sw1_double_tap = false;
-				drvLedSet(DRV_LED_RED, DRV_LED_TOGGLE);
-			}
-			else {
-				break;
-			}
-		}
+/*!
+ * @brief Checks whether the given on-board button have been
+ * pressed, via polling.
+ * @param button The on-board button ID to enable interrupts.
+ * @param int_event The interrupt event that triggers an IRQ.
+ */
+void drvButtonIntEnable(drv_button_id button, drv_interrupt_event int_event) {
+	if(int_event < drvINT_LOGIC_1 || int_event > drvINT_LOGIC_0) {
+		while(1);
 	}
-	drvLedSet(DRV_LED_GREEN, DRV_LED_CLEAR);
-	drvLedSet(DRV_LED_RED, DRV_LED_CLEAR);
+	pin_handler_t *onboard_btn = drvGetButton(button);
+	portConfigInterrupt(onboard_btn, int_event);
+}
+
+/*!
+ * @brief Checks whether the button pin caused an interrupt.
+ * @param button The on-board button ID to check for interrupts.
+ * @retval "true" if an interrupt have occurred, and "false" otherwise.
+ */
+void drvButtonIntClear(drv_button_id button) {
+	pin_handler_t *onboard_btn = drvGetButton(button);
+	portClearInterrupt(onboard_btn);
+}
+
+/*!
+ * @brief Clears the interrupt flag in the button pin.
+ * @param button The on-board button ID that caused the interrupts.
+ */
+bool drvButtonIntCheck(drv_button_id button) {
+	pin_handler_t *onboard_btn = drvGetButton(button);
+	return portCheckInterrupt(onboard_btn);
+}
+
+// =========================================================================
+// PRIVATE FUNCTIONS IMPLEMENTATIONS
+// =========================================================================
+
+/*!
+ * @brief Get the pin handler for the given LED ID.
+ * @param led The desired on-board LED ID.
+ * @retval The pin handler of the given LED.
+ */
+pin_handler_t *drvGetLed(drv_led_id led) {
+	if(led > drvLED_RED || led < drvLED_GREEN) {
+		while(1);
+	}
+	return (led == drvLED_GREEN) ? &drv_led_green : &drv_led_red;
+}
+
+/*!
+ * @brief Get the pin handler for the given button ID.
+ * @param button The desired on-board button ID.
+ * @retval The pin handler of the given button.
+ */
+pin_handler_t *drvGetButton(drv_button_id button) {
+	if(button > drvBUTTON_SW3 || button < drvBUTTON_SW1) {
+		while(1);
+	}
+	return (button == drvBUTTON_SW3) ? &drv_button_sw3 : &drv_button_sw1;
 }
